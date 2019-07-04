1e5334d57bd327174364c4dbbd38a1286fe9eb64
unknown <safirah988@gmail.com>
2019-07-04 15:12:44 +0700

    coba

---
+++ adminLTE/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace adminLTE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Beranda()
+        {
+            return View();
+        }
+        public IActionResult Daftaranggota()
+        {
+            return View();
+        }
+        public IActionResult Publikasi()
+        {
+            return View();
+        }
+        public IActionResult KalenderKegiatan()
+        {
+            return View();
+        }
+    }
+}
