caa9781d13ef022e85d09e0e70bedd2075a14bff
safirah988@gmail.com <safirah988@gmail.com>
2019-07-26 15:44:08 +0700

    Pengajuan Proposal

---
+++ adminLTE/BusinessModel/Combobox.cs
@@ -0,0 +1,87 @@
+﻿using adminLTE.Models;
+using adminLTE.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adminLTE.BusinessModel
+{
+    public class Combobox
+    {
+        private readonly DBINTEGRASI_MASTER_BAYUPPKU2Context _context;
+
+        public Combobox(DBINTEGRASI_MASTER_BAYUPPKU2Context context)
+        {
+            _context = context;
+        }
+
+        public List<ComboboxViewModel> Mahasiswa()
+        {
+            var mhs = from m in _context.Mahasiswa
+                      join o in _context.Orang on m.OrangId equals o.Id
+                      join ms1 in _context.MahasiswaSarjana on m.Id equals ms1.MahasiswaId
+                      where m.Id == ms1.MahasiswaId
+                      select new ComboboxViewModel
+                      {
+                          ID = m.Id.ToString(),
+                          Value = o.Nama
+                      };
+            return mhs.ToList();
+        }
+        public List<ComboboxViewModel> Ormawa()
+        {
+            var orw = from m in _context.OrganisasiOrmawa
+                      select new ComboboxViewModel
+                      {
+                          ID = m.Id.ToString(),
+                          Value = m.Nama
+                      };
+            return orw.ToList();
+        }
+
+        //PengajuanProposal
+        public List<ComboboxViewModel> AnggotaOrmawa()
+        {
+            var Anggota = from m in _context.AnggotaOrmawa
+                          join o in _context.OrganisasiOrmawa on m.OrganisasiOrmawaId equals o.Id
+                          select new ComboboxViewModel
+                          {
+                              ID = m.Id.ToString(),
+                              Value = o.Nama
+                          };
+            return Anggota.ToList();
+                   
+        }
+        public List<ComboboxViewModel> TipeKegiatanOrmawa()
+        { 
+            var TipeKegiatan = from m in _context.TipeKegiatanOrmawa
+                      select new ComboboxViewModel
+                      {
+                          ID = m.Id.ToString(),
+                          Value = m.Nama
+                      };
+            return TipeKegiatan.ToList();
+        }
+        public List<ComboboxViewModel> JenisKegiatanOrmawa()
+        {
+            var JenisKegiatan = from m in _context.JenisKegiatanOrmawa
+                               select new ComboboxViewModel
+                               {
+                                   ID = m.Id.ToString(),
+                                   Value = m.Nama
+                               };
+            return JenisKegiatan.ToList();
+        }
+       /* public List<ComboboxViewModel> PenanggungJawabOrmawa()
+        {
+            var JenisKegiatan = from m in _context.Pen
+                                select new ComboboxViewModel
+                                {
+                                    ID = m.Id.ToString(),
+                                    Value = m.Nama
+                                };
+            return JenisKegiatan.ToList();
+        }*/
+    }
+}
---
+++ adminLTE/Controllers/PengajuanProposalController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using adminLTE.BusinessModel;
+using adminLTE.Models;
+using adminLTE.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace adminLTE.Controllers
+{
+    public class PengajuanProposalController : Controller
+    {
+        private readonly DBINTEGRASI_MASTER_BAYUPPKU2Context _context;
+        private readonly Combobox _combobox;
+
+        public PengajuanProposalController(DBINTEGRASI_MASTER_BAYUPPKU2Context context, Combobox combobox)
+        {
+            _context = context;
+            _combobox = combobox;
+        }
+
+        // GET: Anggota
+        //public async Task<IActionResult> Daftaranggota()
+        //{
+        //    return View(await _context.AnggotaOrmawa.ToListAsync());
+        //}
+        public IActionResult Pengajuan()
+        {
+           PengajuanProposalViewModel vm = new PengajuanProposalViewModel();
+            return View(vm);
+        }
+
+        // GET: Anggota/Create
+        public IActionResult Add()
+        {
+            //if (id == 0)
+            //    return View(new Anggota());
+            //else
+            //    return View(_context.AnggotaOrmawa.Find(id));
+            PengajuanProposalViewModel vm = new PengajuanProposalViewModel();
+            vm.ListAnggotaOrmawa = new SelectList(_combobox.AnggotaOrmawa(), "ID", "Value");
+            vm.ListTipeKegiatan = new SelectList(_combobox.TipeKegiatanOrmawa(), "ID", "Value");
+            vm.ListJenisKegiatan = new SelectList(_combobox.JenisKegiatanOrmawa(), "ID", "Value");
+            //vm.ListPenanggungJawab = new SelectList(_combobox.(), "ID", "Value");
+            return View(vm);
+        }
+
+        // POST: Anggota/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public IActionResult AddOrEdit(PengajuanProposalKegiatan vm)
+        {
+            if (ModelState.IsValid)
+            {
+                //if (anggota.AnggotaID == 0)
+                //    _context.Add(anggota);
+                //else
+                //    _context.Update(anggota);
+                //await _context.SaveChangesAsync();
+                PengajuanProposalKegiatan pengajuan = new PengajuanProposalKegiatan();
+                pengajuan.AnggotaOrmawaId = vm.AnggotaOrmawaId;
+                pengajuan.Kegiatan = vm.Kegiatan;
+                pengajuan.TipeKegiatanOrmawa = vm.TipeKegiatanOrmawa;
+                pengajuan.JenisKegiatanOrmawa = vm.JenisKegiatanOrmawa;
+                pengajuan.DanaAnggaran = vm.DanaAnggaran;
+                pengajuan.PenanggungJawabId = vm.PenanggungJawabId;
+                pengajuan.ApprovedBy = vm.ApprovedBy;
+                pengajuan.TimeApproved = vm.TimeApproved;
+                _context.PengajuanProposalKegiatan.Add(pengajuan);
+                _context.SaveChanges();
+                //return RedirectToAction(nameof(Daftaranggota));
+                return RedirectToAction("Pengajuan", "PengajuanProposal");
+            }
+            return RedirectToAction("Pengajuan", "PengajuanProposal");
+        }
+
+        // GET: Anggota/Delete/5
+       /* public async Task<IActionResult> Delete(int? id)
+        {
+            var anggota = await _context.AnggotaOrmawa.FindAsync(id);
+            _context.AnggotaOrmawa.Remove(anggota);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Daftaranggota));
+        }*/
+
+    }
+}
